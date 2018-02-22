8ed50f98118917d8a1d5e27d742bd38329db8d70
Ron Tang <tzq2008wd@gmail.com>
2018-02-22 16:30:09 +0800

    format

--- GridQuadTree.cs
+++ GridQuadTree.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
@@ -27,131 +27,131 @@ namespace RonTang
         private GridQuadTreeNode<T>[][] nodes;
 
         public GridQuadTree(Vector2 worldCenter, float worldLengthVal, float objMinLengthVal)
+	{
+        	worldRect = CreateRect(worldCenter, new Vector2(worldLengthVal, worldLengthVal));
+		objMinLength = objMinLengthVal;
+		worldLength = worldLengthVal;
+		Init ();
+	}
+
+	public GridQuadTree(Rect worldRectVal,float objMinLengthVal)
+	{
+
+		float maxSide = worldRectVal.width >= worldRectVal.height ? worldRectVal.width: worldRectVal.height;
+		worldRect = CreateRect(worldRectVal.center, new Vector2(maxSide, maxSide));
+		objMinLength = objMinLengthVal;
+		worldLength = maxSide;
+		Init ();
+
+	}
+
+	public void GetItems(Rect resultRect, ref List<GridQuadTreeItem<T>> itemsList)
+	{
+		if ( itemsList!= null)
 		{
-            worldRect = CreateRect(worldCenter, new Vector2(worldLengthVal, worldLengthVal));
-	    	objMinLength = objMinLengthVal;
-	    	worldLength = worldLengthVal;
-	    	Init ();
+			GetItems(rootNode, resultRect, ref itemsList);
 		}
+	}
 
-		public GridQuadTree(Rect worldRectVal,float objMinLengthVal)
-		{
+	private void GetItems(GridQuadTreeNode<T> parentNode, Rect resultRect, ref List<GridQuadTreeItem<T>> itemsList)
+	{
+		parentNode.GetItems (resultRect, ref itemsList);
 
-			float maxSide = worldRectVal.width >= worldRectVal.height ? worldRectVal.width: worldRectVal.height;
-			worldRect = CreateRect(worldRectVal.center, new Vector2(maxSide, maxSide));
-			objMinLength = objMinLengthVal;
-			worldLength = maxSide;
-			Init ();
-
-		}
-
-		public void GetItems(Rect resultRect, ref List<GridQuadTreeItem<T>> itemsList)
-		{
-				if ( itemsList!= null)
-				{
-					GetItems(rootNode, resultRect, ref itemsList);
-				}
-		}
-
-		private void GetItems(GridQuadTreeNode<T> parentNode, Rect resultRect, ref List<GridQuadTreeItem<T>> itemsList)
-		{
-			parentNode.GetItems (resultRect, ref itemsList);
-
-			if (parentNode.IsLeaf || parentNode.ChildrenItemCount == 0)
+		if (parentNode.IsLeaf || parentNode.ChildrenItemCount == 0)
 			return;
 
-			var nodeTopLeft = nodes[parentNode.NodeLevel + 1][parentNode.TopLeftIndex];
-			if (nodeTopLeft.looseRect.Overlaps(resultRect))
-				GetItems(nodeTopLeft, resultRect, ref itemsList);
+		var nodeTopLeft = nodes[parentNode.NodeLevel + 1][parentNode.TopLeftIndex];
+		if (nodeTopLeft.looseRect.Overlaps(resultRect))
+			GetItems(nodeTopLeft, resultRect, ref itemsList);
 
-			var nodeTopRight = nodes[parentNode.NodeLevel + 1][parentNode.TopRightIndex];
-			if (nodeTopRight.looseRect.Overlaps(resultRect))
-				GetItems(nodeTopRight, resultRect, ref itemsList);
+		var nodeTopRight = nodes[parentNode.NodeLevel + 1][parentNode.TopRightIndex];
+		if (nodeTopRight.looseRect.Overlaps(resultRect))
+			GetItems(nodeTopRight, resultRect, ref itemsList);
 
-			var nodeBottomLeft = nodes[parentNode.NodeLevel + 1][parentNode.BottomLeftIndex];
-			if (nodeBottomLeft.looseRect.Overlaps(resultRect))
-				GetItems(nodeBottomLeft,resultRect,ref itemsList);
+		var nodeBottomLeft = nodes[parentNode.NodeLevel + 1][parentNode.BottomLeftIndex];
+		if (nodeBottomLeft.looseRect.Overlaps(resultRect))
+			GetItems(nodeBottomLeft,resultRect,ref itemsList);
 
-			var nodeBottomRight = nodes[parentNode.NodeLevel + 1][parentNode.BottomRightIndex];
-			if (nodeBottomRight.looseRect.Overlaps(resultRect))
-				GetItems(nodeBottomRight, resultRect, ref itemsList);
+		var nodeBottomRight = nodes[parentNode.NodeLevel + 1][parentNode.BottomRightIndex];
+		if (nodeBottomRight.looseRect.Overlaps(resultRect))
+			GetItems(nodeBottomRight, resultRect, ref itemsList);
 
-		}
+	}
 
-		public bool Insert(GridQuadTreeItem<T> item)
+	public bool Insert(GridQuadTreeItem<T> item)
+	{
+		float maxSide = item.ItemRect.width >= item.ItemRect.height ? item.ItemRect.width : item.ItemRect.height;
+		if (item.ItemRect.height > worldLength || item.ItemRect.width > worldLength) 
 		{
-			float maxSide = item.ItemRect.width >= item.ItemRect.height ? item.ItemRect.width : item.ItemRect.height;
-			if (item.ItemRect.height > worldLength || item.ItemRect.width > worldLength) 
+			Debug.LogError ("Item rect too big, can not insert it");
+			return false;
+		}
+		int level = Mathf.FloorToInt (Mathf.Log (worldLength / maxSide, 2f));
+		if (level > nodeMaxLevel) 
+		{
+			level = nodeMaxLevel;
+			Debug.LogWarning ("Maybe this item is too small, but we can add it to max level.");
+		}
+		Vector2 itemLocalPos = item.Position - worldRect.min;
+		int sideNodeCount = (int)Mathf.Pow(2, level);
+		float cellLength = worldLength / sideNodeCount;
+		int column = (int)(itemLocalPos.x / cellLength);
+		int raw = (int)(itemLocalPos.y / cellLength);
+		int index = raw * sideNodeCount + column;
+
+		if (nodes[level][index].nodeRect.Contains(item.Position))
+		{
+			var nodeToAdd = nodes[level][index];
+			if (!nodeToAdd.IsLeaf)
 			{
-				Debug.LogError ("Item rect too big, can not insert it");
-				return false;
+				var leftTop = nodes[nodeToAdd.NodeLevel + 1][nodeToAdd.TopLeftIndex];
+				var rightTop = nodes[nodeToAdd.NodeLevel + 1][nodeToAdd.TopRightIndex];
+				var leftBottom = nodes[nodeToAdd.NodeLevel + 1][nodeToAdd.BottomLeftIndex]; 
+				var rightBottom = nodes[nodeToAdd.NodeLevel + 1][nodeToAdd.BottomRightIndex];
+				if (leftTop.looseRect.Contains(item.ItemRect))
+					nodeToAdd = leftTop;
+				else if (rightTop.looseRect.Contains(item.ItemRect))
+					nodeToAdd = rightTop;
+				else if (leftBottom.looseRect.Contains(item.ItemRect))
+					nodeToAdd = leftBottom;
+				else if (rightBottom.looseRect.Contains(item.ItemRect))
+					nodeToAdd = rightBottom;
+
 			}
-			int level = Mathf.FloorToInt (Mathf.Log (worldLength / maxSide, 2f));
-			if (level > nodeMaxLevel) 
-			{
-				level = nodeMaxLevel;
-				Debug.LogWarning ("Maybe this item is too small, but we can add it to max level.");
-			}
-				Vector2 itemLocalPos = item.Position - worldRect.min;
-				int sideNodeCount = (int)Mathf.Pow(2, level);
-				float cellLength = worldLength / sideNodeCount;
-				int column = (int)(itemLocalPos.x / cellLength);
-				int raw = (int)(itemLocalPos.y / cellLength);
-				int index = raw * sideNodeCount + column;
-
-				if (nodes[level][index].nodeRect.Contains(item.Position))
-				{
-					var nodeToAdd = nodes[level][index];
-					if (!nodeToAdd.IsLeaf)
-					{
-						var leftTop = nodes[nodeToAdd.NodeLevel + 1][nodeToAdd.TopLeftIndex];
-						var rightTop = nodes[nodeToAdd.NodeLevel + 1][nodeToAdd.TopRightIndex];
-						var leftBottom = nodes[nodeToAdd.NodeLevel + 1][nodeToAdd.BottomLeftIndex]; 
-						var rightBottom = nodes[nodeToAdd.NodeLevel + 1][nodeToAdd.BottomRightIndex];
-						if (leftTop.looseRect.Contains(item.ItemRect))
-							nodeToAdd = leftTop;
-						else if (rightTop.looseRect.Contains(item.ItemRect))
-							nodeToAdd = rightTop;
-						else if (leftBottom.looseRect.Contains(item.ItemRect))
-							nodeToAdd = leftBottom;
-						else if (rightBottom.looseRect.Contains(item.ItemRect))
-							nodeToAdd = rightBottom;
-
-					}
-					nodeToAdd.Insert(item);
-					ChangeAllParentChildrenItemCount(nodeToAdd, 1);
-					Debug.Log("insert.....");
-					return true;
-				}
-
-				return false;
+			nodeToAdd.Insert(item);
+			ChangeAllParentChildrenItemCount(nodeToAdd, 1);
+			Debug.Log("insert.....");
+			return true;
 		}
 
-		private void ChangeAllParentChildrenItemCount(GridQuadTreeNode<T> node, int deltaVal)
+		return false;
+	}
+
+	private void ChangeAllParentChildrenItemCount(GridQuadTreeNode<T> node, int deltaVal)
+	{
+		int pIndex = node.ParentIndex;
+		int pLevel = node.NodeLevel - 1;
+		for ( ; pLevel >= 0; pLevel--)
 		{
-			  int pIndex = node.ParentIndex;
-			  int pLevel = node.NodeLevel - 1;
-			  for ( ; pLevel >= 0; pLevel--)
-			  {
-				   nodes[pLevel][pIndex].ChildrenItemCount += deltaVal;
-				   pIndex = nodes[pLevel][pIndex].ParentIndex;
-			  }
+			 nodes[pLevel][pIndex].ChildrenItemCount += deltaVal;
+			 pIndex = nodes[pLevel][pIndex].ParentIndex;
 		}
+	}
 
-		private void ItemMove(GridQuadTreeItem<T> item, GridQuadTreeNode<T> node)
-		{
-				ChangeAllParentChildrenItemCount(node, -1);
-				Insert (item);
-		}
+	private void ItemMove(GridQuadTreeItem<T> item, GridQuadTreeNode<T> node)
+	{
+		ChangeAllParentChildrenItemCount(node, -1);
+		Insert (item);
+	}
 
 
-		private void Init()
-		{
-			nodeMaxLevel = Mathf.FloorToInt(Mathf.Log(worldLength / objMinLength, 2f) + 1);
-			InitGrid();
-			BulidTree();
-			rootNode = nodes[0][0];
-		}
+	private void Init()
+	{
+		nodeMaxLevel = Mathf.FloorToInt(Mathf.Log(worldLength / objMinLength, 2f) + 1);
+		InitGrid();
+		BulidTree();
+		rootNode = nodes[0][0];
+	}
 
         private void InitGrid()
         {
